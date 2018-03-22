fe4c7ad6aeaf738d01001e1b6c4d5cf68ea789c6
Liz <elizabethmpedley@gmail.com>
2018-03-22 11:20:14 -0500

    created variables

---
+++ variable.js
@@ -0,0 +1,21 @@
+var num1 = 3.14159;
+var num2 = -77;
+var num3 = 42;
+var num4 = 400000000000;
+var string1 = 'Johnny';
+var string2 = 'Jessica';
+var string3 = 'Jake';
+var isReal = true;
+var isNotReal = false;
+var undefinedVariable;
+
+console.log('Number 1 is ' + num1);
+console.log('Number 2 is ' + num2);
+console.log('Number 3 is ' + num3);
+console.log('Number 4 is ' + num4);
+console.log('String 1 is ' + string1);
+console.log('String 2 is ' + string2);
+console.log('String 3 is ' + string3);
+console.log('My first boolean is ' + isReal);
+console.log('My second boolean is ' + isNotReal);
+console.log('My undefined variable is ' + undefinedVariable);
